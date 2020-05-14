b306515c0a93449406fd9b1692c6f9cb6c780cb7
José Andrés Abad. <64880801+JAAC8@users.noreply.github.com>
2020-05-13 23:35:57 -0500

    Inicialización del Proyecto

--- src/main/java/ec/edu/ups/test/Test.java
+++ src/main/java/ec/edu/ups/test/Test.java
@@ -5,10 +5,41 @@
  */
 package ec.edu.ups.test;
 
+import java.util.Scanner;
+import ec.edu.ups.modelo.Persona;
 /**
  *
  * @author José Andrés Abad
  */
 public class Test {
+    public static void main(String args[]){
+        Scanner scann = new Scanner(System.in);
+        
+        System.out.println("REGISTRO");
+        System.out.println("Ingrese su:"+"\nCédula: ");
+        String cedula = scann.next();
+        System.out.println("nNombre:");
+        String nombre = scann.next();
+        System.out.println("Apellido:");
+        String apellido = scann.next();
+        System.out.println("Correo:");
+        String correo = scann.next();
+        System.out.println("Contraseña:");
+        String contraseña = scann.next();
+        Persona usuario = new Persona(cedula, nombre, apellido, correo, contraseña);
+        
+        System.out.println("INICIAR SESIÓN");//*
+        System.out.println("Correo:");
+        String correoIs = scann.next();
+        System.out.println("Contraseña:");
+        String contraseñaIs= scann.next();
+        
+        //Comparación con equals medainte Dao/Controlador        
+        //if(){}
+            boolean repetidor1=true;
+            while(repetidor1){
+            
+            }
+    }
     
 }
